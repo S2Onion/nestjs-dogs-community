a7e4c3219d4bdfe6166cd2bc5e0a688a6a6528d6
SusungKim <sskim@deleo.co.kr>
2022-06-24 16:36:43 +0900

    Class validator 파이프 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // Class validator 기능 파이프 추가
   await app.listen(process.env.APPLICATION_PORT);
 }
 bootstrap();
