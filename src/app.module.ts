80a0b5d45e56bf2dabfc617802d17252eaac4c5a
SusungKim <sskim@deleo.co.kr>
2022-06-24 15:51:55 +0900

    DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true, // mongodb url을 읽을 수 있도록 설정
+      useUnifiedTopology: true, // 최신 mongodb 드라이버 엔진을 사용하도록 설정
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
