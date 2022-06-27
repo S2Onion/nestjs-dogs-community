e7b28dea0de1ecff997356b7b8ed01a765aa85f6
SusungKim <sskim@deleo.co.kr>
2022-06-27 09:38:23 +0900

    셋팅 변경 dogs 관련 기능 개발 중

--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -1,29 +1,31 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { DogRequestDto } from './dto/dogs.request.dto';
 
 @Controller('dogs')
 export class DogsController {
-  @Get()
-  getCurrentDog() {
-    return 'current dog';
-  }
+	@Get()
+	getCurrentDog() {
+		return 'current dog';
+	}
 
-  @Post()
-  async signUp() {
-    return 'signup';
-  }
+	@Post()
+	async signUp(@Body() body: DogRequestDto) {
+		console.log(body);
+		return 'signup';
+	}
 
-  @Post('login')
-  logIn() {
-    return 'login';
-  }
+	@Post('login')
+	logIn() {
+		return 'login';
+	}
 
-  @Post('logout')
-  logOut() {
-    return 'logout';
-  }
+	@Post('logout')
+	logOut() {
+		return 'logout';
+	}
 
-  @Post('upload/dogs')
-  uploadDogImg() {
-    return 'upload dog image';
-  }
+	@Post('upload/dogs')
+	uploadDogImg() {
+		return 'upload dog image';
+	}
 }
